0e02c20c619d5f9ec7c7ef2094b96ed32c9145d4
Ricardo Henrique Soares <ricardohsti@gmail.com>
2018-01-18 17:52:56 -0400

    Factories e Seeders para Users, Thread e Reply

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -13,11 +13,33 @@ use Faker\Generator as Faker;
 |
 */
 
-$factory->define(App\User::class, function (Faker $faker) {
+$factory->define(App\Models\User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
         'remember_token' => str_random(10),
+    ];
+});
+
+$factory->define(App\Models\Thread::class, function (Faker $faker) {
+    return [
+        'title' => $faker->sentence,
+        'body' => implode(' ', $faker->paragraphs),
+        'user_id' => function () {
+            return factory(App\Models\User::class)->create()->id;
+        },
+    ];
+});
+
+$factory->define(App\Models\Reply::class, function (Faker $faker) {
+    return [
+        'body' => implode(' ', $faker->paragraphs),
+        'user_id' => function () {
+            return factory(App\Models\User::class)->create()->id;
+        },
+        'thread_id' => function () {
+            return factory(App\Models\Thread::class)->create()->id;
+        },
     ];
 });
---
+++ database/seeds/RepliesTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class RepliesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $threads = factory(App\Models\Thread::class, 50)->create();
+
+        $threads->each(function ($thread) {
+            factory(App\Models\Reply::class, rand(5, 10))->create([
+                'thread_id' => $thread->id
+            ]);
+        });
+    }
+}
